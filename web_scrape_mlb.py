f7ca45b984af063ed11e258be6147b4533b1bd9a
bszek213 <bszekely@nevada.unr.edu>
2023-03-29 22:38:37 -0700

    Init Commit

---
+++ web_scrape_mlb.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+html parse code - MLB
+@author: brianszekely
+"""
+import requests
+from bs4 import BeautifulSoup
+from pandas import DataFrame
+from numpy import nan
+from time import sleep
+from os.path import join, exists
+from os import getcwd
+from urllib import request
+from urllib.request import Request, urlopen
+from pandas import read_csv
+from numpy import where
+from re import search
+from difflib import get_close_matches
+
+def get_data_team(team,year):
+    #example url: https://www.baseball-reference.com/teams/tgl.cgi?team=NYY&t=b&year=2022
+    #Batting URL
+    URL_b = f'https://www.baseball-reference.com/teams/tgl.cgi?team={team}&t=b&year={year}'
+    hdr = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
+    req_1 = Request(URL_b,headers=hdr)
+    html_1 = request.urlopen(req_1)
+    soup_bat = BeautifulSoup(html_1, "html.parser")
+    sleep(3)
+    #Pitching
+    URL_p = f'https://www.baseball-reference.com/teams/tgl.cgi?team={team}&t=p&year={year}'
+    hdr = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
+    req_1 = Request(URL_p,headers=hdr)
+    html_1 = request.urlopen(req_1)
+    soup_pitch = BeautifulSoup(html_1, "html.parser")
+
+    #Batting data
+    game_result = []
+    PA = []
+    AB = []
+    R = []
+    H = []
+    second_base = []
+    third_base = []
+    HR = []
+    RBI = []
+    BB = []
+    IBB = []
+    SO = []
+    HBP = []
+    SH = []
+    SF = []
+    ROE = []
+    GIDP = []
+    SB = []
+    CS = []
+    batting_avg = []
+    table = soup_bat.find(id='all_team_batting_gamelogs')
+    tbody = table.find('tbody')
+    tr_body = tbody.find_all('tr')
+    for trb in tr_body:
+        for td in trb.find_all('td'):
+            if td.get('data-stat') == "game_result":
+                if 'W' in td.get_text():
+                    game_result.append(1)
+                else:
+                    game_result.append(0)
+            if td.get('data-stat') == 'PA':
+                PA.append(td.get_text())
+            if td.get('data-stat') == 'AB':
+                AB.append(td.get_text())
+            if td.get('data-stat') == 'R':
+                R.append(td.get_text())
+            if td.get('data-stat') == 'H':
+                H.append(td.get_text())
+            if td.get('data-stat') == '2B':
+                second_base.append(td.get_text())
+            if td.get('data-stat') == '3B':
+                third_base.append(td.get_text())
+            if td.get('data-stat') == 'HR':
+                HR.append(td.get_text())
+            if td.get('data-stat') == 'RBI':
+                RBI.append(td.get_text())
+            if td.get('data-stat') == 'BB':
+                BB.append(td.get_text())
+            if td.get('data-stat') == 'IBB':
+                IBB.append(td.get_text())
+            if td.get('data-stat') == 'SO':
+                SO.append(td.get_text())
+            if td.get('data-stat') == 'HBP':
+                HBP.append(td.get_text())
+            if td.get('data-stat') == 'SH':
+                SH.append(td.get_text())
+            if td.get('data-stat') == 'SF':
+                SF.append(td.get_text())
+            if td.get('data-stat') == 'ROE':
+                ROE.append(td.get_text())
+            if td.get('data-stat') == 'GIDP':
+                GIDP.append(td.get_text())
+            if td.get('data-stat') == 'SB':
+                SB.append(td.get_text())
+            if td.get('data-stat') == 'CS':
+                CS.append(td.get_text())
+            if td.get('data-stat') == 'batting_avg':
+                batting_avg.append(td.get_text())
+    print(game_result)
+    
+get_data_team('NYY',2022)
